1db081537b8f7cfca8f067e702b7c1fd99a40261
Alireza <alirezafazel379@gmail.com>
2024-12-04 10:32:09 +0100

    Initial Commit

---
+++ Learn ML/Learn_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_MLApp.swift
+//  Learn ML
+//
+//  Created by Alireza Fazel on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
